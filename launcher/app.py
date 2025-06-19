93b768aeb2eab2822b7c2819a5d4b128a56196bf
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-06-19 20:41:37 +0530

    Update app.py

--- launcher/app.py
+++ launcher/app.py
@@ -136,4 +136,3 @@ class Launcher(App):
         self.log(f'ready to start intent {j_entrypoint} {j_orientation}')
         activity.startActivity(intent)
         self.log('activity started')
-        System.exit(0)
