dabe295eb1698da8550afa01a2b30094603b4b35
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-06-20 13:40:20 +0530

    Update LauncherActivity.java

--- src/org/launcher/android/LauncherActivity.java
+++ src/org/launcher/android/LauncherActivity.java
@@ -1,6 +1,8 @@
 package org.launcher.android;
 
+import android.content.Intent;
 import android.util.Log;
+
 import org.kivy.android.PythonActivity;
 
 public class LauncherActivity extends PythonActivity {
@@ -8,14 +10,10 @@ public class LauncherActivity extends PythonActivity {
 
     @Override
     public void onBackPressed() {
-        // Optionally finish this activity
         super.onBackPressed();
 
-        // Start the main activity
         Intent intent = new Intent(this, org.kivy.android.PythonActivity.class);
-
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         startActivity(intent);
     }
-
 }
