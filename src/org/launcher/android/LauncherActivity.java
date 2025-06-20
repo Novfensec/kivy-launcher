18b0718e9db76dab89d7040a079438d119d343de
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-06-20 12:01:18 +0530

    Update LauncherActivity.java

--- src/org/launcher/android/LauncherActivity.java
+++ src/org/launcher/android/LauncherActivity.java
@@ -1,20 +1,20 @@
 package org.launcher.android;
 
+import android.util.Log;
 import org.kivy.android.PythonActivity;
-
 
 public class LauncherActivity extends PythonActivity {
     private static final String TAG = "LauncherActivity";
 
     @Override
     public void onBackPressed() {
-        Log.i("SDL", "Back button pressed");
+        Log.i(TAG, "Back button pressed");
 
         // Call PythonActivity's custom handler
         if (this instanceof PythonActivity) {
             ((PythonActivity) this).handleBackPressed();
             return;
-    }
+        }
 
         super.onBackPressed();
     }
